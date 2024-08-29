4fd0fd960a3060a8f8f2fbdf0418a1f1abca3c79
reg499 <newregtemp@outlook.com>
2024-08-29 23:48:08 +0400

    check

---
+++ check.js
@@ -0,0 +1,27 @@
+const { ethers } = require("ethers");
+
+const mnemonic =
+  "12 MNEMONIC";
+
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://mainnet.infura.io/v3/YOURAPIKEY"
+);
+
+async function main() {
+  try {
+    const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+    const address = wallet.address;
+
+    console.log(`Address: ${address}`);
+
+    const balance = await provider.getBalance(address);
+
+    const balanceInEther = ethers.utils.formatEther(balance);
+
+    console.log(`Balance: ${balanceInEther} ETH`);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+main();
